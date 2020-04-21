38e248c16a795967d03bd6fa23cea27e1fd242bf
Roger Alvarez <rogeralvarez3@gmail.com>
2020-04-21 15:36:20 -0600

    respaldo

--- src/components/maestro-detalle.vue
+++ src/components/maestro-detalle.vue
@@ -1,51 +1,92 @@
 <template>
-    <v-app>
-        <v-card width="800">
-            <v-card-title class="blue-grey lighten-4"><h3>{{título}}</h3></v-card-title>
-            <v-divider></v-divider>
-            <v-card-text>
-                <div>
-                    <v-text-field v-for="(item,key) in maestro" v-model="maestro[key]" :key="key" :label="key"></v-text-field>
-                    <v-data-table :items="detalle" hide-actions :headers="headers_detalle">
-                        <template v-slot:props="items">
-                            <td v-for="(field,i) in headers_detalle" :key="i">{{props.item[field]}}</td>
-                        </template>
-                    </v-data-table>
-                </div>
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions>
-                <v-btn class="primary"><v-icon>fa fa-save</v-icon>&nbsp;guardar</v-btn>
-                <v-btn class="primary"><v-icon>fa fa-print</v-icon>&nbsp;guardar e imprimir</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-app>
+  <v-app>
+    <v-card width="800">
+      <v-card-title class="blue-grey lighten-4">
+        <h3>{{título}}</h3>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-row>
+          <v-text-field
+            v-for="item in textFieldsMaestro"
+            v-model="item.valor"
+            :key="item.value"
+            :label="item.text"
+            :width="300"
+            class="mr-2 xs-1"
+          ></v-text-field>
+          <v-select
+            v-for="item in selectFieldsMaestro"
+            :items="item.control.items"
+            :key="item.value"
+            :width="item.width"
+            class="mr-2 xs-1"
+          ></v-select>
+          <v-data-table :items="detalle.campos" hide-actions :headers="headers_detalle">
+            <template v-slot:props="items">
+              <td v-for="(field,i) in headers_detalle" :key="i">{{props.item[field]}}</td>
+            </template>
+          </v-data-table>
+        </v-row>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn>
+          <v-icon>fa fa-save</v-icon>&nbsp;guardar
+        </v-btn>
+        <v-btn>
+          <v-icon>fa fa-print</v-icon>&nbsp;guardar e imprimir
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-dialog>
+      <v-card>
+        <v-card-title></v-card-title>
+      </v-card>
+    </v-dialog>
+  </v-app>
 </template>
 
 <script>
 export default {
-    props:{
-        título:String,
-        maestro:Object,
-        detalle:Array
-    },
-    data(){
-        return{
-            
+  props: {
+    título: String,
+    maestro: Object,
+    detalle: Object
+  },
+  data() {
+    return {
+      showAddDetail: false
+    };
+  },
+  methods: {
+    agregarADetalle: function() {}
+  },
+  computed: {
+    headers_detalle: function() {
+      var result = [];
+      this.detalle.campos.forEach(campo => {
+        if (campo.text) {
+          result.push(campo);
         }
+      });
+      return result;
     },
-    computed:{
-        headers_detalle:function(){
-            var mv=this
-            if(mv.detalle.length===0){return[]}
-            var result=[]
-            Object.keys(mv.detalle[0]).forEach(k=>{result.push({value:k,text:k})})
-            return result
-        }
+    textFieldsMaestro: function() {
+      var result = this.maestro.campos.filter(c => {
+        return !c.control;
+      });
+      return result;
+    },
+    selectFieldsMaestro: function() {
+      var result = this.maestro.campos.filter(c => {
+        return c.control;
+      });
+      return result;
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/views/Facturación.vue
+++ src/views/Facturación.vue
@@ -1,24 +1,47 @@
 <template>
-    <v-app>
-        <maestro-detalle título="facturación" :maestro="maestro" :detalle="detalle"></maestro-detalle>
-    </v-app>
+  <v-app>
+    <maestro-detalle título="facturación" :maestro="maestro" :detalle="detalle"></maestro-detalle>
+  </v-app>
 </template>
 
 <script>
-import maestroDetalle from '../components/maestro-detalle'
+import maestroDetalle from "../components/maestro-detalle";
 export default {
-    components:{maestroDetalle},
-    data(){
-        return{
-            maestro:{Cliente:''},
-            detalle:[
-                {Producto:'',Cantidad:'',Precio:'',Subtotal:''}
-            ]
-        }
-    }
-}
+  components: { maestroDetalle },
+  data() {
+    return {
+      maestro: {
+        tabla: "ventas",
+        campos: [
+          { value: "cliente", text: "Cliente", width: 300 },
+          {
+            value: "tipo",
+            text: "Tipo",
+            control: {
+              tipo: "select",
+              items: [
+                { value: 1, text: "Al contado" },
+                { value: 2, text: "Al crédito" }
+              ]
+            },
+            width: 150
+          }
+        ]
+      },
+      detalle: {
+        tabla: "detalle de ventas",
+        campos: [
+          { value: "ventasId", text: "" },
+          { value: "producto", text: "Producto" },
+          { value: "cantidad", text: "Cantidad", align: "center" },
+          { value: "precio", text: "Precio", align: "end" },
+          { value: "subtotal", text: "Subtotal", align: "end" }
+        ]
+      }
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
